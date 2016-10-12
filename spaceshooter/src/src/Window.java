6b8533abab35665dc67243872b481d99922d06a8
aah5307 <aah5307@IST203-106.win.psu.edu>
2016-10-12 16:42:13 -0400

    changed Window class to extend JFrame

--- spaceshooter/src/src/Window.java
+++ spaceshooter/src/src/Window.java
@@ -16,23 +16,22 @@ import javax.swing.JFrame;
  */
 
 // Main Window for game, implements JFrame, Takes Game as constructor
-public class Window extends Canvas{
+public class Window extends JFrame{
     
     private static final double serialVersionID =  1.00;
     
     public Window(int width, int height, String title, Game game){
-        JFrame frame = new JFrame(title);
         
-        frame.setPreferredSize(new Dimension(width,height));
-        frame.setMaximumSize(new Dimension(width,height));
-        frame.setMinimumSize(new Dimension(width,height));
+        this.setPreferredSize(new Dimension(width,height));
+        this.setMaximumSize(new Dimension(width,height));
+        this.setMinimumSize(new Dimension(width,height));
         
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
         
-        frame.add(game);
-        frame.setVisible(true);
+        this.add(game);
+        this.setVisible(true);
         game.start();
     }
 }
